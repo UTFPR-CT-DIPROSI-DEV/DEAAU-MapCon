e1a341cb97e8c28f54b999829df2ed6577b0ca63
Jhouny <jhonyaraujo9121@gmail.com>
2024-07-30 16:33:19 -0300

    .

--- BASH/sync_folder.sh
+++ BASH/sync_folder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=/mnt/e/Users/jhony/Documents/Jhony/Universidade/UTFPR/MAPCON
+DIR=/mnt/d/Joule/Documents/Jhony/Universidade/UTFPR/IC/Sistema-MAPCON
 HOME=/home/todos/alunos/ct/a2358859/
 
-sudo ${DIR}/BASH/rsync.sh ${DIR}/BASH/private_key.pem a2358859 ${DIR}/Sistema-MAPCON/mapcon/mapcon/ a2358859@conflitosurbanos.cogeti.ct.internal:${HOME}/mapcon/
+sudo ${DIR}/BASH/rsync.sh ${DIR}/BASH/private_key.pem a2358859 ${DIR}/mapcon/mapcon/build/ a2358859@conflitosurbanos.cogeti.ct.internal:${HOME}/mapcon/
